95fb7d2b711f83198491a1c499cc17c0ae568608
dmitry <dmitry.b@scopicsoftware.com>
2020-03-11 18:56:48 +0200

    Initial commit

---
+++ src/main/java/com/learn/maven/SomeClass.java
@@ -0,0 +1,7 @@
+package com.learn.maven;
+
+public class SomeClass {
+    public void print() {
+        System.out.println("dfdf");
+    }
+}
---
+++ src/test/java/com/learn/maven/SomeClassTest.java
@@ -0,0 +1,11 @@
+package com.learn.maven;
+
+import org.junit.Test;
+
+public class SomeClassTest {
+    @Test
+    public void testPrint() {
+        SomeClass someClass = new SomeClass();
+        someClass.print();
+    }
+}
